955563b101446f26dfc01267940113772e40f012
Ibrahim Younes Zaidi <145864227+Ibrahim-Zaidi@users.noreply.github.com>
2023-12-07 16:51:06 +0100

    Day_? :)

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const decrease = document.querySelector('.decrease');
+const reset = document.querySelector('.reset');
+const increase = document.querySelector('.increase');
+const value = document.getElementById('value');
+const btn = document.querySelectorAll('.btn');
+
+let i;
+
+ 
+
+
+
